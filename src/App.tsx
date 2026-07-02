c74e4db8c863161bc923d5f8dc16cb8a5e680247
Denis Kinyanjui ~Bluetech Kenya <denisnjorogek@gmail.com>
2026-07-02 20:06:58 +0300

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import About from './components/About'
-import Connect from './components/Connect'
-import Education from './components/Education'
-import Experience from './components/Experience'
+
 import Footer from './components/Footer'
 import Hero from './components/Hero'
-import Interests from './components/Interests'
+
 import Navbar from './components/Navbar'
 
 function App() {
